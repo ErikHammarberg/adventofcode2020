6d59c3ea2acf46f8f3cdb23397a320b442114868
Erik Hammarberg Almén (mzq498) <erik.hammarberg-almen@teliacompany.com>
2020-12-03 08:00:19 +0100

    Refactor

--- src/com/xmas/DayTree.java
+++ src/com/xmas/DayTree.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class DayTree {
 
-  public int puzzleOne(String input ) {
+  public int puzzleOne(String input) {
+    return puzzleCounter(input, 3, 1);
+  }
+
+  public int puzzleCounter(String input, int right, int down ) {
     int result = 0;
     int position = 0;
     var slope = input.split("\\n");
-    for(int i = 1; i < slope.length; i ++) {
-      position = (position + 3) % slope[i].length();
+    for(int i = 1; i < slope.length; i += down) {
+      position = (position + right) % slope[i].length();
       if(slope[i].charAt(position) == '#') {
         ++result;
       }
