dfdc200c9da5586e8536e1e19ed2e4f7b0008e56
slaeggan <slaggan@gmail.com>
2021-12-01 21:53:52 +0100

    13 one done

---
+++ test/com/xmas/DayThirteenTest.java
@@ -0,0 +1,31 @@
+package com.xmas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DayThirteenTest {
+
+    DayThirteen sut = new DayThirteen();
+
+    @Test
+    public void exampleTestOne() {
+        int result = sut.solveOne(exampleInput);
+        assertEquals(295, result);
+    }
+
+    @Test
+    public void testOne() {
+        int result = sut.solveOne(input);
+        assertEquals(5946, result);
+        System.out.println(result);
+    }
+
+    String exampleInput =
+            "939\n" +
+            "7,13,x,x,59,x,31,19";
+
+    String input =
+            "1008832\n" +
+                    "23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,19,x,x,x,x,x,x,x,x,x,29,x,991,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17\n";
+}
